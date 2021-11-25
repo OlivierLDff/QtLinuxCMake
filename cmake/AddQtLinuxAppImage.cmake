426bcdb7abe6293e05025447dce00367c6b192fc
Ahmed Yarub Hani Al Nuaimi <ahmed.alnuaimi@zwift.com>
2021-11-25 12:18:09 -0300

    Qt6 adaptations

--- cmake/AddQtLinuxAppImage.cmake
+++ cmake/AddQtLinuxAppImage.cmake
@@ -9,43 +9,56 @@ set(QT_LINUX_QT_ROOT ${QT_LINUX_QT_ROOT})
 set(QT_LINUX_SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR})
 
 if(NOT QT_LINUX_DEPLOY_APP)
-  find_program(QT_LINUX_DEPLOY_APP linuxdeployqt)
+  find_program(QT_LINUX_DEPLOY_APP linuxdeploy-x86_64.AppImage HINTS ${CMAKE_CURRENT_BINARY_DIR})
   if(QT_LINUX_DEPLOY_APP)
-    message(STATUS "Found linuxdeployqt : ${QT_LINUX_DEPLOY_APP}")
+    message(STATUS "Found linuxdeploy-x86_64.AppImage : ${QT_LINUX_DEPLOY_APP}")
   else()
-    set(QT_LINUX_DEPLOY_APP ${CMAKE_CURRENT_BINARY_DIR}/linuxdeployqt)
-    message(STATUS "Fail to find linuxdeployqt, download from https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage to ${QT_LINUX_DEPLOY_APP}")
-    file(DOWNLOAD "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage" ${CMAKE_CURRENT_BINARY_DIR}/tmp/linuxdeployqt)
+    set(QT_LINUX_DEPLOY_APP ${CMAKE_CURRENT_BINARY_DIR}/linuxdeploy-x86_64.AppImage)
+    message(STATUS "Failed to find linuxdeploy-x86_64.AppImage, download from https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage to ${QT_LINUX_DEPLOY_APP}")
+    file(DOWNLOAD "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage" ${CMAKE_CURRENT_BINARY_DIR}/tmp/linuxdeploy-x86_64.AppImage)
 
     file(
-      COPY ${CMAKE_CURRENT_BINARY_DIR}/tmp/linuxdeployqt
+      COPY ${CMAKE_CURRENT_BINARY_DIR}/tmp/linuxdeploy-x86_64.AppImage
       DESTINATION ${CMAKE_CURRENT_BINARY_DIR}
       FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
     )
 
-    file(REMOVE ${CMAKE_CURRENT_BINARY_DIR}/tmp/linuxdeployqt)
+    file(REMOVE ${CMAKE_CURRENT_BINARY_DIR}/tmp/linuxdeploy-x86_64.AppImage)
+  endif()
+endif()
+
+if(NOT QT_LINUX_DEPLOY_PLUGIN)
+  find_program(QT_LINUX_DEPLOY_PLUGIN linuxdeploy-plugin-qt-x86_64.AppImage HINTS ${CMAKE_CURRENT_BINARY_DIR})
+  if(QT_LINUX_DEPLOY_PLUGIN)
+    message(STATUS "Found linuxdeploy-plugin-qt-x86_64.AppImage : ${QT_LINUX_DEPLOY_PLUGIN}")
+  else()
+    set(QT_LINUX_DEPLOY_PLUGIN ${CMAKE_CURRENT_BINARY_DIR}/linuxdeploy-plugin-qt-x86_64.AppImage)
+    message(STATUS "Failed to find linuxdeploy-plugin-qt-x86_64.AppImage, download from https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage to ${QT_LINUX_DEPLOY_PLUGIN}")
+    file(DOWNLOAD "https://github.com/linuxdeploy/linuxdeploy-plugin-qt/releases/download/continuous/linuxdeploy-plugin-qt-x86_64.AppImage" ${CMAKE_CURRENT_BINARY_DIR}/tmp/linuxdeploy-plugin-qt-x86_64.AppImage)
+
+    file(
+            COPY ${CMAKE_CURRENT_BINARY_DIR}/tmp/linuxdeploy-plugin-qt-x86_64.AppImage
+            DESTINATION ${CMAKE_CURRENT_BINARY_DIR}
+            FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
+    )
+
+    file(REMOVE ${CMAKE_CURRENT_BINARY_DIR}/tmp/linuxdeploy-plugin-qt-x86_64.AppImage)
   endif()
 endif()
 
 include(CMakeParseArguments)
 
-# CMake function that wrap linuxdeployqt (https://github.com/probonopd/linuxdeployqt)
 function(add_qt_linux_appimage TARGET)
 
   set(QT_LINUX_OPTIONS
     ALL
-    NO_TRANSLATIONS
-    NO_PLUGINS
-    NO_STRIP
     NO_APPIMAGE
     NO_COPYRIGHT_FILES
-    ALLOW_ENVIRONMENT_VARIABLE
   )
 
   set(QT_LINUX_ONE_VALUE_ARG
     APP_DIR
     QML_DIR
-    VERSION
     VERBOSE_LEVEL
   )
 
@@ -57,7 +70,7 @@ function(add_qt_linux_appimage TARGET)
   cmake_parse_arguments(ARGIMG "${QT_LINUX_OPTIONS}" "${QT_LINUX_ONE_VALUE_ARG}" "${QT_LINUX_MULTI_VALUE_ARG}" ${ARGN})
 
   if(NOT ARGIMG_APP_DIR)
-    message(FATAL_ERROR "add_qt_linux_appimage : APP_DIR no specified")
+    message(FATAL_ERROR "add_qt_linux_appimage : appdir not specified")
   endif()
 
   # Check AppDir exists
@@ -75,22 +88,13 @@ function(add_qt_linux_appimage TARGET)
   set(QT_LINUX_QMAKE_PATH ${QT_LINUX_QT_ROOT}/bin/qmake)
   set(QT_LINUX_QMAKE_OPT -qmake=${QT_LINUX_QMAKE_PATH})
 
+  # --output
   if(NOT ARGIMG_NO_APPIMAGE)
-    set(QT_LINUX_APPIMAGE_OPT -appimage)
+    set(QT_LINUX_APPIMAGE_OPT --output appimage)
   endif()
 
-  # -no-translations
-  if(ARGIMG_NO_TRANSLATIONS)
-    set(QT_LINUX_TRANSLATIONS_OPT -no-translations)
-  endif()
-
-  # -no-plugins
-  if(ARGIMG_NO_PLUGINS)
-    set(QT_LINUX_PLUGINS_OPT -no-plugins)
-  endif()
-
-  # -extra-plugins
-  # args are comma separated
+  # EXTRA_QT_PLUGINS
+  # args are semicolon separated
   if(ARGIMG_EXTRA_PLUGINS)
     set(EXTRA_PLUGINS_LIST "")
     foreach(PLUGIN ${ARGIMG_EXTRA_PLUGINS})
@@ -98,41 +102,27 @@ function(add_qt_linux_appimage TARGET)
       if(EXTRA_PLUGINS_LIST STREQUAL "")
         set(EXTRA_PLUGINS_LIST "${PLUGIN}")
       else()
-        string(APPEND EXTRA_PLUGINS_LIST ",${PLUGIN}")
+        string(APPEND EXTRA_PLUGINS_LIST ";${PLUGIN}")
       endif()
     endforeach()
     message(STATUS "Extra plugins list : ${EXTRA_PLUGINS_LIST}")
-    set(QT_LINUX_EXTRA_PLUGINS_OPT "-extra-plugins=${EXTRA_PLUGINS_LIST}")
+    set(QT_LINUX_EXTRA_PLUGINS_OPT export EXTRA_QT_PLUGINS=${EXTRA_PLUGINS_LIST} &&)
   endif()
 
-  # -no-strip
-  if(ARGIMG_NO_STRIP)
-    set(QT_LINUX_STRIP_OPT -no-strip)
-  endif()
-
-  # -no-strip
+  # DISABLE_COPYRIGHT_FILES_DEPLOYMENT
   if(ARGIMG_NO_COPYRIGHT_FILES)
-    set(QT_LINUX_COPYRIGHT_OPT -no-copy-copyright-files)
+    set(QT_LINUX_COPYRIGHT_OPT export DISABLE_COPYRIGHT_FILES_DEPLOYMENT=1 &&)
   endif()
 
-  # -qmldir
+  # QML_SOURCES_PATHS
   if(ARGIMG_QML_DIR)
-    set(QT_LINUX_QML_DIR_OPT -qmldir=${ARGIMG_QML_DIR})
+    set(QT_LINUX_QML_DIR_OPT export QML_SOURCES_PATHS=${ARGIMG_QML_DIR} &&)
   endif()
 
-  # -verbose
+  # --verbosity
   if(ARGIMG_VERBOSE_LEVEL)
-    set(QT_LINUX_VERBOSE_OPT -verbose=${ARGIMG_VERBOSE_LEVEL})
-  endif()
-
-  if(ARGIMG_VERSION)
-    set(QT_LINUX_APP_VERSION ${ARGIMG_VERSION})
-  else()
-    if(PROJECT_VERSION)
-      set(QT_LINUX_APP_VERSION ${PROJECT_VERSION})
-    else()
-      set(QT_LINUX_APP_VERSION "1.0.0")
-    endif()
+    set(QT_LINUX_VERBOSE_OPT "--verbosity ${ARGIMG_VERBOSE_LEVEL}")
+    set(QT_LINUX_PLUGIN_VERBOSE_OPT export DEBUG=${ARGIMG_VERBOSE_LEVEL} &&)
   endif()
 
   if(ARGIMG_ALL)
@@ -153,17 +143,8 @@ function(add_qt_linux_appimage TARGET)
     message(STATUS "QT_LINUX_DEPLOY_APP : ${QT_LINUX_DEPLOY_APP}")
     message(STATUS "QT_LINUX_APPIMAGE_OPT : ${QT_LINUX_APPIMAGE_OPT}")
     message(STATUS "QT_LINUX_QML_DIR_OPT : ${QT_LINUX_QML_DIR_OPT}")
-    message(STATUS "QT_LINUX_TRANSLATIONS_OPT : ${QT_LINUX_TRANSLATIONS_OPT}")
-    message(STATUS "QT_LINUX_PLUGINS_OPT : ${QT_LINUX_PLUGINS_OPT}")
     message(STATUS "QT_LINUX_EXTRA_PLUGINS_OPT : ${QT_LINUX_EXTRA_PLUGINS_OPT}")
-    message(STATUS "QT_LINUX_STRIP_OPT : ${QT_LINUX_STRIP_OPT}")
     message(STATUS "QT_LINUX_VERBOSE_OPT : ${QT_LINUX_VERBOSE_OPT}")
-    message(STATUS "QT_LINUX_APP_VERSION : ${QT_LINUX_APP_VERSION}")
-    message(STATUS "ALLOW_ENVIRONMENT_VARIABLE : ${ALLOW_ENVIRONMENT_VARIABLE}")
-    message(STATUS "NO_TRANSLATIONS : ${ARGIMGNO_TRANSLATIONS}")
-    message(STATUS "NO_PLUGINS : ${ARGIMGNO_PLUGINS}")
-    message(STATUS "NO_STRIP : ${ARGIMGNO_STRIP}")
-    message(STATUS "NO_APPIMAGE : ${ARGIMGNO_APPIMAGE}")
     message(STATUS "NO_COPYRIGHT_FILES : ${ARGIMGNO_COPYRIGHT_FILES}")
   endif()
 
@@ -174,20 +155,18 @@ function(add_qt_linux_appimage TARGET)
     COMMAND ${CMAKE_COMMAND} -DALLOW_ENVIRONMENT_VARIABLE=${ALLOW_ENVIRONMENT_VARIABLE} -DAPPRUN_PATH=${CMAKE_CURRENT_BINARY_DIR}/$<CONFIG>/AppDir -DTARGET_FILE_NAME=$<TARGET_FILE_NAME:${TARGET}> -P "${QT_LINUX_SOURCE_DIR}/GenerateAppRun.cmake"
     COMMAND ${CMAKE_COMMAND} -E copy_directory ${ARGIMG_APP_DIR} ${CMAKE_CURRENT_BINARY_DIR}/$<CONFIG>/AppDir
     COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${TARGET}> ${CMAKE_CURRENT_BINARY_DIR}/$<CONFIG>/AppDir/usr/bin/$<TARGET_FILE_NAME:${TARGET}>
-    COMMAND ${CMAKE_COMMAND} -E env VERSION=${QT_LINUX_APP_VERSION}
-      ${QT_LINUX_DEPLOY_APP}
-      # Desktop file
-      ${CMAKE_CURRENT_BINARY_DIR}/$<CONFIG>/AppDir/${QT_LINUX_DESKTOP_FILE}
-      # Options
-      ${QT_LINUX_APPIMAGE_OPT}
-      ${QT_LINUX_QML_DIR_OPT}
-      ${QT_LINUX_TRANSLATIONS_OPT}
-      ${QT_LINUX_PLUGINS_OPT}
-      ${QT_LINUX_EXTRA_PLUGINS_OPT}
-      ${QT_LINUX_STRIP_OPT}
-      ${QT_LINUX_COPYRIGHT_OPT}
-      -verbose=3
-      -unsupported-allow-new-glibc
+    COMMAND ${QT_LINUX_QML_DIR_OPT}
+            ${QT_LINUX_COPYRIGHT_OPT}
+            ${QT_LINUX_PLUGIN_VERBOSE_OPT}
+            ${QT_LINUX_EXTRA_PLUGINS_OPT}
+            export PATH=/usr/bin/:${QT_LINUX_QT_ROOT}/bin/:${QT_LINUX_QT_ROOT}/libexec/ &&
+            ${QT_LINUX_DEPLOY_APP}
+            --appdir $<CONFIG>/AppDir
+            -d ${CMAKE_CURRENT_BINARY_DIR}/$<CONFIG>/AppDir/${QT_LINUX_DESKTOP_FILE}
+            -e $<CONFIG>/CloudCompanionStandalone
+            ${QT_LINUX_VERBOSE_OPT}
+            --plugin qt
+            ${QT_LINUX_APPIMAGE_OPT}
 
     COMMENT "Deploy AppImage with ${QT_LINUX_DEPLOY_APP}. Source are in ${QT_LINUX_SOURCE_DIR}"
   )
